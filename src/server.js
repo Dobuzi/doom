bfbd1999c9321b238f3e99885c89457774a0dfb1
Dobuzi <kimjw7981@gmail.com>
2021-08-29 13:51:39 +0900

    install socket io

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import http from "http"
-import WebSocket from "ws"
+import SocketIO from "socket.io"
 import express from "express";
 
 const app = express();
@@ -15,10 +15,15 @@ app.get("/*", (_, res) => res.redirect("/"));
 const port = 3000
 const handleListen = () => console.log(`Listening on http://localhost:${port}`); 
 
-const server = http.createServer(app)
+const httpServer = http.createServer(app)
+const io = SocketIO(httpServer)
 
+io.on("connection", (socket) => {
+    console.log(socket)
+})
+
+/*
 const wss = new WebSocket.Server({ server })
-
 const sockets = []
 
 wss.on("connection", (socket) => {
@@ -39,5 +44,6 @@ wss.on("connection", (socket) => {
         }
     })
 })
+*/
 
-server.listen(port, handleListen)
+httpServer.listen(port, handleListen)
