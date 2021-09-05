e3b824fd6e247246a91c1e380851b63c4bef20fb
Dobuzi <kimjw7981@gmail.com>
2021-09-05 13:32:45 +0900

    apply socket io

--- src/server.js
+++ src/server.js
@@ -19,31 +19,12 @@ const httpServer = http.createServer(app)
 const io = SocketIO(httpServer)
 
 io.on("connection", (socket) => {
-    console.log(socket)
-})
-
-/*
-const wss = new WebSocket.Server({ server })
-const sockets = []
-
-wss.on("connection", (socket) => {
-    socket["nickname"] = "Anonymous"
-    sockets.push(socket)
-    console.log("Connected to Browser 😍")
-    socket.on("close", () => console.log("Disconneted from the Browser ❎"))
-    socket.on("message", (message) => {
-        const parsedMessage = JSON.parse(message)
-        console.log(parsedMessage)
-        switch (parsedMessage.type) {
-            case "nickname":
-                socket["nickname"] = parsedMessage.payload
-                break
-            case "new_message":
-                sockets.forEach((s) => s.send(`${socket.nickname}: ${parsedMessage.payload}`))
-                break
-        }
+    socket.on("enter_room", (msg, done) => {
+        console.log(msg)
+        setTimeout(() => {
+            done()
+        }, 3000)
     })
 })
-*/
 
 httpServer.listen(port, handleListen)
